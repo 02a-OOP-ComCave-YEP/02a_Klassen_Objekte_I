33bbc425ce9afe8f0a882bf26522e5599095eca4
PiSera1 <kevinwellens24@gmail.com>
2025-01-16 10:44:09 +0100

    // 1. Iteration Objekte durch new erzeugen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,10 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+            
+            // 1. Iteration Objekte durch new erzeugen
+            System.out.println(new Cat());
+            System.out.println(new Cat());
 
 
     }
